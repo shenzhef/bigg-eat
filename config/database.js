99ea22ef06740707ffe6bf4442747ce8acacc754
Martin Miauro <martin.miauro@gmail.com>
2022-03-31 11:23:17 -0300

    changed database access

--- config/database.js
+++ config/database.js
@@ -1,10 +1,23 @@
-const path = require('path');
+const path = require("path");
 
 module.exports = ({ env }) => ({
   connection: {
-    client: 'sqlite',
+    client: "postgres",
     connection: {
-      filename: path.join(__dirname, '..', env('DATABASE_FILENAME', '.tmp/data.db')),
+      port: env.int("DATABASE_PORT", 25060),
+      database: env("DATABASE_NAME", "db-eat"),
+      user: env("DATABASE_USERNAME", "db-eat"),
+      password: env("DATABASE_PASSWORD", "kAXUIS8DOkzvvHSE"),
+      host: env(
+        "DATABASE_HOST",
+        "app-146eac7f-6ccc-492f-a220-59f2b43bd15b-do-user-11248998-0.b.db.ondigitalocean.com"
+      ),
+
+      // filename: path.join(
+      //   __dirname,
+      //   "..",
+      //   env("DATABASE_FILENAME", "./tmp/data.db")
+      // ),
     },
     useNullAsDefault: true,
   },
